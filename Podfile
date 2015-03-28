151ad8718071b4b62d9b3956d2d1cfd1e4459b0b
Connor Giles <connorgiles@icloud.com>
2015-03-28 00:27:44 -0400

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+
+pod 'Spring', '~> 1.0.1'
+use_frameworks!
---
+++ Sift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sift
+//
+//  Created by Connor Giles on 2015-03-28.
+//  Copyright (c) 2015 Connor Giles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
