dd217d5ddc23f14d78b72685de588cdabd799bb0
Connor Giles <connorgiles@icloud.com>
2016-10-29 23:32:56 -0400

    .gitignore is now working

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '7.0'
-
-pod 'pop', '~> 1.0'
-pod 'SVProgressHUD'
-pod 'SDWebImage'
-pod 'SVPullToRefresh'
-#pod 'Spring', '~> 1.0.1'
-use_frameworks!
--- Sift/Sift-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "SVPullToRefresh/SVPullToRefresh.h"
-#import "SDWebImage/UIImageView+WebCache.h"
-#import "SVProgressHUD/SVProgressHUD.h"
-#import "iOS-API/GTLSift.h"
-#import "pop/POP.h"
